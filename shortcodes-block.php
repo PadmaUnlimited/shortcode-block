0e191e2f8c22eeccf73625db39082f3f0d6ced5b
Sharon <sharon@plasma.cr>
2018-01-17 14:29:45 -0600

    cambio de nombre de autor

--- shortcodes-block.php
+++ shortcodes-block.php
@@ -4,7 +4,7 @@ Plugin Name: Padma Shortcodes Block
 Plugin URI: http://plasma.cr/
 Description: Padma Shortcode block, based on Shortcode block for Headway 1.0.0.2
 Version: 1.0.0
-Author: Sharon Umaña
+Author: Plasma
 Author URI: http://www.plasma.cr
 License: GNU GPL v2
 */
