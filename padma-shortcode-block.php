501298ecf12f8b0190b548c9000fce22ce7e0078
Manfred <marodok@gmail.com>
2019-06-11 12:52:27 -0600

    Auto Update fixes

--- padma-shortcode-block.php
+++ padma-shortcode-block.php
@@ -3,7 +3,7 @@
 Plugin Name: Padma Shortcode Block
 Plugin URI: https://www.padmaunlimited.com/plugins/shortcode-block
 Description: Padma Shortcode block, based on Shortcode block for Headway 1.0.0.2
-Version: 1.0.8
+Version: 1.0.9
 Author: Padma Unlimited
 Author URI: https://www.padmaunlimited.com/
 License: GNU GPL v2
@@ -27,14 +27,4 @@ function register_shortcode_block() {
 	
 	return padma_register_block('PadmaShortcodesBlock', substr(WP_PLUGIN_URL . '/' . str_replace(basename(__FILE__), '', plugin_basename(__FILE__)), 0, -1));
 
-}
-
-// Updates
-if(is_admin()){
-	add_action('after_setup_theme', 'padma_shortcode_block_updates');
-	function padma_shortcode_block_updates(){
-		if ( ! empty ( $GLOBALS[ 'PadmaUpdater' ] ) ){
-		    $GLOBALS[ 'PadmaUpdater' ]->updater('padma-shortcode-block',__DIR__);
-		}
-	}
 }
