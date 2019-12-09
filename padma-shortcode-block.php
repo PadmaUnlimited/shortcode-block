0b59c9f18c92b62ed93c961b2187d06459b511d6
Manfred <marodok@gmail.com>
2019-12-09 14:39:30 -0600

    New icons

--- padma-shortcode-block.php
+++ padma-shortcode-block.php
@@ -3,7 +3,7 @@
 Plugin Name: Padma Shortcode Block
 Plugin URI: https://www.padmaunlimited.com/plugins/shortcode-block
 Description: Padma Shortcode block, based on Shortcode block for Headway 1.0.0.2
-Version: 1.0.10
+Version: 1.0.11
 Author: Padma Unlimited
 Author URI: https://www.padmaunlimited.com/
 License: GNU GPL v2
@@ -21,7 +21,19 @@ function register_shortcode_block() {
 	if (!class_exists('PadmaBlockAPI') )
 		return false;
 
-	
-	return padma_register_block('PadmaShortcodesBlock', substr(WP_PLUGIN_URL . '/' . str_replace(basename(__FILE__), '', plugin_basename(__FILE__)), 0, -1));
+	$class = 'PadmaShortcodesBlock';
+	$block_type_url = substr(WP_PLUGIN_URL . '/' . str_replace(basename(__FILE__), '', plugin_basename(__FILE__)), 0, -1);		
+	$class_file = __DIR__ . '/block.php';
+	$icons = array(
+			'path' => __DIR__,
+			'url' => $block_type_url
+		);
+
+	return padma_register_block(
+		$class,
+		$block_type_url,
+		$class_file,
+		$icons
+	);
 
 }
